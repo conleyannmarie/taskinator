7f7abaaf6cdf8b41e37491a30d9174fdee5b83ef
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-08-12 15:52:59 -0600

    added js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form")
+console.log(formEl);
 
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+var createTaskHandler = function(event) {
 
-buttonEl.addEventListener("click", function() {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+};
+
+formEl.addEventListener("submit", createTaskHandler)
+
